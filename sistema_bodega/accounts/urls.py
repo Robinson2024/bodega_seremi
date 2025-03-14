bb13ff5ccfded09a41d640ba9ee7cefcc5cbd2e9
Robinson Bravo <robinsonbravo97@gmail.com>
2025-03-14 11:58:37 -0300

    login autenticado, e interfaz post logeo implementada, inicio y cerrar sesion

--- sistema_bodega/accounts/urls.py
+++ sistema_bodega/accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('login/', views.CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='/accounts/login/'), name='logout'),  # Redirige al login después de cerrar sesión
 ]
--- sistema_bodega/accounts/views.py
+++ sistema_bodega/accounts/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
 from django.contrib.auth.views import LoginView
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def home(request):
     return render(request, 'accounts/home.html')
 
 class CustomLoginView(LoginView):
-    template_name = 'accounts/login.html'  # Usa tu template existente
+    template_name = 'accounts/login.html'
+
+    def post(self, request, *args, **kwargs):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(self.get_success_url())
+        else:
+            messages.error(request, 'Credenciales inválidas')
+            return self.render_to_response(self.get_context_data())
+
+    def get_success_url(self):
+        return '/'
